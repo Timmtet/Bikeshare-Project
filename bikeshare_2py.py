f4c59dbfdd0e3bff898d90af1b7a68e7bf053503
Timileyin Akintilo <96595056+Timmtet@users.noreply.github.com>
2023-01-20 11:16:02 +0100

    Update bikeshare_2py.py

--- bikeshare_2py.py
+++ bikeshare_2py.py
@@ -7,7 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-de get_filters():
+def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
 
